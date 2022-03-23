e293e3fb1df37a6bab4abd03d2ac1f612eff8a87
luamz <luamsilva@id.uff.br>
2022-03-23 10:44:40 -0300

    Gui login internationalized

--- interfaces/GUI_login.py
+++ interfaces/GUI_login.py
@@ -9,9 +9,19 @@ class LoginGUI:
     USER_TYPE = {1: "convidado", 2: "premium"}
 
     def __init__(self):
+        self.current_language = 'EN'
         self.client = Client()
         self.client_gui = None
         self.window = Tk()
+        print()
+
+        self.language_frame = Frame(self.window)
+        self.language_button = Button(self.language_frame, text="EN", width='1', height="1",
+                                      bg="#b36200", state=DISABLED, disabledforeground="#faddc8",
+                                      command=lambda: self.change_language("EN"))
+        self.language_button2 = Button(self.language_frame, text="PT", width='1', height="1",
+                                       bg="orange", foreground="white", disabledforeground="#faddc8",
+                                       command=lambda: self.change_language("PT"))
 
         self.login_frame = Frame(self.window)
         self.logo = Frame(self.login_frame)
@@ -19,24 +29,30 @@ class LoginGUI:
         self.logo_label = Label(self.logo, image=self.img)
 
         self.name_frame = Frame(self.login_frame)
-        self.name_label = Label(self.name_frame, text="Nome")
+        self.name_label = Label(self.name_frame, text=NAME.get(self.current_language))
         self.name_input = Entry(self.name_frame, width=25, font="Helvetica 12 bold")
 
         self.type_frame = Frame(self.login_frame)
         self.option_input = IntVar()
-        self.radio_button1 = Radiobutton(self.type_frame, text="Convidado", variable=self.option_input,
-                                         value=1, selectcolor='#dedede')
+        self.radio_button1 = Radiobutton(self.type_frame, text=GUEST.get(self.current_language),
+                                         variable=self.option_input, value=1, selectcolor='#dedede')
         self.radio_button2 = Radiobutton(self.type_frame, text="Premium", variable=self.option_input,
                                          value=2, selectcolor='#dedede')
 
         self.menu_frame = Frame(self.login_frame)
         self.output_label = Label(self.menu_frame, text="")
-        self.login_button = Button(self.menu_frame, text="Entrar", width=12, height=2, bg="orange", command=self.login)
+        self.login_button = Button(self.menu_frame, text=LOG_IN.get(self.current_language),
+                                   width=12, height=2, bg="orange", command=self.login)
 
     def configure(self):
         self.window.title('Biting Wire - Login')
         self.window.configure(background='white')
         self.window.geometry(str(self.WIDTH) + "x" + str(self.HEIGHT))
+
+        self.language_frame.pack(pady=10, padx=(250, 0))
+        self.language_frame.configure(background='white')
+        self.language_button.pack(side=LEFT)
+        self.language_button2.pack()
 
         self.login_frame.pack()
         self.login_frame.configure(background='white')
@@ -74,10 +90,10 @@ class LoginGUI:
         option = self.option_input.get()
 
         if not name:
-            self.output_label.config(text='Nome não pode ficar em branco!', foreground='red')
+            self.output_label.config(text=NAME_BLANK.get(self.current_language), foreground='red')
         else:
             self.client.login(name, self.USER_TYPE.get(option))
-            self.window.title('Biting Wire - Seu Programa de Streaming Favorito')
+            self.window.title(WELCOME.get(self.current_language))
             self.window.geometry("750x510")
             self.login_frame.destroy()
             self.client_gui = ClientGUI(self.window, self.client)
@@ -87,6 +103,24 @@ class LoginGUI:
     def render(self):
         self.configure()
         self.window.mainloop()
+
+    def change_language(self, language):
+        if self.current_language == language:
+            pass
+        else:
+            if self.current_language == "EN":
+                self.current_language = "PT"
+                self.language_button.configure(bg="orange", state=NORMAL, disabledforeground="#faddc8")
+                self.language_button2.configure(bg="#b36200", state=DISABLED, foreground="white")
+
+            elif self.current_language == "PT":
+                self.current_language = "EN"
+                self.language_button.configure(bg="#b36200", state=DISABLED, foreground="white")
+                self.language_button2.configure(bg="orange", state=NORMAL, disabledforeground="#faddc8")
+
+            self.name_label.configure(text=NAME.get(self.current_language))
+            self.radio_button1.configure(text=GUEST.get(self.current_language))
+            self.login_button.configure(text=LOG_IN.get(self.current_language))
 
 
 def main():
---
+++ interfaces/locale.py
@@ -0,0 +1,5 @@
+LOG_IN = {'PT': 'Entrar', 'EN': 'Log In'}
+NAME = {'PT': 'Nome', 'EN': 'Nickname'}
+NAME_BLANK = {'PT': 'Nome não pode ficar em branco!', 'EN': "Name can't be blank"}
+GUEST = {'PT': 'Convidado', 'EN': 'Guest'}
+WELCOME = {'PT': 'Biting Wire - Seu Programa de Streaming Favorito', 'EN': 'Biting Wire - Your favorite Streaming App'}
